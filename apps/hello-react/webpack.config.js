c5cecee9b1ee2b4026f7e8d69030a8832f90a8e8
hanhhn <hngochanh@outlook.com>
2025-05-31 09:06:27 +0700

    Update

--- apps/hello-react/webpack.config.js
+++ apps/hello-react/webpack.config.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    path: join(__dirname, '../../dist/apps/hello-react'),
   },
   devServer: {
     port: 4200,
